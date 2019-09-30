a6f8762ff851b91d7398adbd0602c378ae8c95b0
huy.do <huy.do@niteco.se>
2019-09-30 17:08:28 +0700

    Update Controller

--- Niteco.EluxApiReflection/Controllers/StockController.cs
+++ Niteco.EluxApiReflection/Controllers/StockController.cs
@@ -6,7 +6,7 @@ namespace Niteco.EluxApiReflection.Controllers
     {
         public double GetStockAvailable(string emailAddress,string modelNumber,string ean)
         {
-            return EluxApiIntHelper.GetStockAvailable(string emailAddress,string modelNumber,string ean);
+            return EluxApiIntHelper.GetStockAvailable(emailAddress, modelNumber,ean);
         }
     }
 }
